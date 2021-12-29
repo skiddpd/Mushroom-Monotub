ecbba7599d821563e0ef7830d28722857165f6ee
skiddpd <95869000+skiddpd@users.noreply.github.com>
2021-12-29 11:24:44 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
+# mushroom monotub project is meant to work with a physical mushroom monotub and 
+# will employ a heater, temp/humidity sensor and LED strip, information will report 
+# back to IFTTT and will ideally be controlled by NodeRed. This code is developed 
+# by Payton "Paul" Daugereau. 
+# Project notes can be found at: https://github.com/skiddpd/Mushroom-Monotubhttps://github.com/skiddpd/Mushroom-Monotub
 
